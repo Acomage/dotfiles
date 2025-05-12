7dec1d224288bfa539422fa779b4520fca6398be
Acomage <acomage468@gmail.com>
2025-05-12 16:12:47 +0800

    save

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,5 @@
+# this file is just casually used. Since Mason upstreams update to 2.0.0 but lazyvim have not migrated yet
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
