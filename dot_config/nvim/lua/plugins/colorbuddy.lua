cfd4e18433def1fea1931618c193192e71c5ba93
Acomage <acomage468@gmail.com>
2025-06-29 11:25:06 +0800

    remove ras logo in dashboard

--- dot_config/nvim/lua/plugins/colorbuddy.lua
+++ dot_config/nvim/lua/plugins/colorbuddy.lua
@@ -6,26 +6,4 @@ return {
       colorscheme = "ras",
     },
   },
-  {
-    "folke/snacks.nvim",
-    ---@type snacks.Config
-    opts = {
-      dashboard = {
-        sections = {
-          {
-            section = "terminal",
-            -- cmd = "chafa ~/.config/nvim/colors/ras.png --format symbols --symbols block --size 60x17 --stretch; sleep .1",
-            cmd = "cat ~/.config/nvim/colors/ras.ans; sleep 0.1",
-            height = 14,
-            padding = 1,
-          },
-          {
-            pane = 1,
-            { section = "keys", gap = 1, padding = 1 },
-            { section = "startup" },
-          },
-        },
-      },
-    },
-  },
 }
