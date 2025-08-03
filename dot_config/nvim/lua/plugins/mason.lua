f64d26c1b900cdd2fb7d645cfa9822c50ebbddee
Acomage <acomage468@gmail.com>
2025-08-03 22:34:01 +0800

    remove useless

--- dot_config/nvim/lua/plugins/mason.lua
+++
@@ -1,5 +0,0 @@
-# this file is just casually used. Since Mason upstreams update to 2.0.0 but lazyvim have not migrated yet
-return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
